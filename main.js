9c24d9bcf12664256bdfa52b5bd2ef0f75c2400e
ZackMacTavish <zmactavish@gmail.com>
2020-05-08 14:41:17 -0400

    added second shape

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let scene, camera, renderer, cube;
+let scene, camera, renderer, cube, dodecahedron;
 
 function init() {
 
@@ -20,22 +20,39 @@ function init() {
         renderer.setSize(window.innerWidth, window.innerHeight);
     
         document.body.appendChild(renderer.domElement);
+
+        ///////CUBE 
         
        const texture = new THREE.TextureLoader().load('textures/6.jpg');
 
 	// Create material with texture
     const material = new THREE.MeshBasicMaterial({ map: texture });
-    
 
-        //const material = new THREE.MeshBasicMaterial({ color: 0x3a89ff });
-        const geometry = new THREE.BoxGeometry( 2, 2, 2);
-        //const geometry = new THREE.DodecahedronGeometry( 1, 0, 0);
+    const geometry = new THREE.BoxGeometry( 3, 3, 3);
+    
+ //const material = new THREE.MeshBasicMaterial({ color: 0x3a89ff });
+
+ cube = new THREE.Mesh( geometry, material );
+ scene.add( cube );
+ cube.position.x += 5;
+ cube.position.y += -3.5;
+
+ ///////////2nd GEOMETRY
+        
+      const geometry1 = new THREE.DodecahedronGeometry( 1, 0, 0);
+      const texture2 = new THREE.TextureLoader().load('textures/6.jpg');
+        const material1 = new THREE.MeshBasicMaterial({ map: texture2 });
         
         
-        cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
+        //dodecahedron.position.y += 1;
+        //dodecahedron.position.x += 1;
+
+        dodecahedron = new THREE.Mesh( geometry1, material1 );
+        scene.add( dodecahedron );
     
     camera.position.z = 5;
+    //camera.position.x = 1;
+    //camera.position.y = 1;
 
 }
 
@@ -44,9 +61,13 @@ function init() {
 
 function animate() {
     requestAnimationFrame( animate );
+
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+
     
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.005;
+    cube.rotation.y += 0.005;
 
 	renderer.render( scene, camera );
 }
